d45b4e32616f5c2c3e0727e152bf408e7d21d8bb
rsneal01 <rsneal01@gmail.com>
2022-02-26 09:39:25 -0800

    commit tree 080aebbe4c8dc9f27eb2d2a11107ba43010fae4b

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,17 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director,index) => (
+        <div key={index}>
+          <p>Director name: {director.name}</p>
+          <ul>
+            {director.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
